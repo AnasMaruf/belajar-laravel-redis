54014ea15fb74737304aa94751d8952632a7377b
AnasMaruf <anasmaruf1101@gmail.com>
2024-12-25 18:37:07 +0700

    Initial Commit

---
+++ tests/Feature/RedisTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Redis;
+use Tests\TestCase;
+
+class RedisTest extends TestCase
+{
+    public function testPing(){
+        $reponse = Redis::command('PING');
+        $this->assertEquals("PONG",$reponse);
+
+        $response = Redis::ping();
+        $this->assertEquals("PONG",$response);
+    }
+
+    public function testString(){
+        Redis::setex("name",2,"anas");
+        $response = Redis::get("name");
+        $this->assertEquals("anas",$response);
+
+        sleep(5);
+
+        $response = Redis::get("name");
+        $this->assertEquals(null,$response);
+    }
+
+    public function testList(){
+        Redis::del("names");
+        Redis::rpush("names","muhammad");
+        Redis::rpush("names","anas");
+        Redis::rpush("names","maruf");
+
+        $response = Redis::lrange("names",0,-1);
+        self::assertEquals(["muhammad","anas","maruf"],$response);
+
+        self::assertEquals("muhammad", Redis::lpop("names"));
+        self::assertEquals("anas", Redis::lpop("names"));
+        self::assertEquals("maruf", Redis::rpop("names"));
+    }
+
+    public function testSet(){
+        Redis::del("names");
+
+        Redis::sadd("names","muhammad");
+        Redis::sadd("names","muhammad");
+        Redis::sadd("names","anas");
+        Redis::sadd("names","anas");
+        Redis::sadd("names","maruf");
+        Redis::sadd("names","maruf");
+
+        $response = Redis::smembers("names");
+        self::assertEquals(["muhammad","anas","maruf"],$response);
+    }
+
+    public function testSortedSet()
+    {
+        Redis::del("names");
+        Redis::zadd("names",100,"muhammad");
+        Redis::zadd("names",80,"anas");
+        Redis::zadd("names",70,"maruf");
+
+        // $response = Redis::zrange("names",0,-1);
+        $response = Redis::zrevrange("names",0,-1);
+        self::assertEquals(["muhammad","anas","maruf"],$response);
+    }
+}
